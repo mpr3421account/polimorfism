351590dc3d5bef52908fcaf4648de367c2141266
Marcos Paulo <itsolutions031250@gmail.com>
2022-09-23 08:39:37 -0300

    polimorfism02

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,17 +7,14 @@ import entities.SavingsAccount;
 public class Program {
     public static void main(String[] args) {
 
-        Account acc1 = new Account(1001, "maria", 1000.0);
-        acc1.withdraw(200.0);
-        System.out.println(acc1.getBalance());
+        Account x = new Account(1001, "maria", 1000.0);
+        Account y = new SavingsAccount(1002, "joao", 1000.0, 0.01);
 
-        Account acc2 = new SavingsAccount(1002, "anna", 1000.0, 0.01);
-        acc2.withdraw(200.0);
-        System.out.println(acc2.getBalance());
+        x.withdraw(50.0);
+        y.withdraw(50.0);
 
-        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-        acc3.withdraw(200.0);
-        System.out.println(acc3.getBalance());
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
 
     }
 }
