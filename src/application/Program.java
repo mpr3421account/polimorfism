14bcbe40e424a1d021ecac51bfe54ec25d50f38f
Marcos Paulo <itsolutions031250@gmail.com>
2022-10-04 16:21:46 -0300

    abstract with polimorfism

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,20 +4,33 @@ import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
 
-        Account x = new Account(1001, "maria", 1000.0);//classe abstract não pode ser instanciada
-        Account y = new SavingsAccount(1002, "joao", 1000.0, 0.01);
-        Account z = new BusinessAccount(1003, "bob", 1000.0, 500.0);
+        List<Account> list = new ArrayList<>();
 
-        x.withdraw(50.0);
-        y.withdraw(50.0);
-        z.withdraw(50.0);
+        list.add(new SavingsAccount(1001, "marcio", 1000.0, 0.01));
+        list.add(new BusinessAccount(1002, "joao", 500.0, 400.0));
+        list.add(new SavingsAccount(1004, "jose", 500.0, 0.01));
+        list.add(new BusinessAccount(1005, "anna", 300.0, 600.0));
 
-        System.out.println(x.getBalance());
-        System.out.println(y.getBalance());
-        System.out.println(z.getBalance());
+        double sum = 0.0;
+        for (Account acc : list){
+            sum += acc.getBalance();
+        }
+
+        System.out.printf("Total balance: %.2f%n", sum);
+
+        for (Account acc : list){
+            acc.deposit(10.0);
+        }
+        for (Account acc : list){
+            System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+        }
+
 
     }
 }
