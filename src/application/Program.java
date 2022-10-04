d2f9bd937d9c89abce0975c42bac5098bba2292a
Marcos Paulo <itsolutions031250@gmail.com>
2022-10-04 10:16:28 -0300

    abstract

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,14 +7,17 @@ import entities.SavingsAccount;
 public class Program {
     public static void main(String[] args) {
 
-        Account x = new Account(1001, "maria", 1000.0);
+        Account x = new Account(1001, "maria", 1000.0);//classe abstract não pode ser instanciada
         Account y = new SavingsAccount(1002, "joao", 1000.0, 0.01);
+        Account z = new BusinessAccount(1003, "bob", 1000.0, 500.0);
 
         x.withdraw(50.0);
         y.withdraw(50.0);
+        z.withdraw(50.0);
 
         System.out.println(x.getBalance());
         System.out.println(y.getBalance());
+        System.out.println(z.getBalance());
 
     }
 }
